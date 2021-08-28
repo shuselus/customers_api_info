c985797791108023af6b11a34bc5ac76e41e80f9
Leonid Shuster <lenchaus@gmail.com>
2021-08-28 14:12:55 +0300

    added currentSectionMap to apDataReducer,  GridCell click handler

--- src/reducers/appDataReducer.js
+++ src/reducers/appDataReducer.js
@@ -1,12 +1,15 @@
-import { SECTION_NAME } from "../actions/appActions";
+import { SECTION_NAME, CURRENT_SECTION_MAP } from "../actions/appActions";
 const initialState = {
-  sectionName: ""
+  sectionName: "",
+  currentSectionMap: new Map()
 }
 
 const appDataReducer = (state = initialState , action) => {
   switch (action.type) {
     case SECTION_NAME:
       return {...state, sectionName: action.name};
+    case CURRENT_SECTION_MAP:
+        return {...state, currentSectionMap: new Map(action.data)};
     default:
       return state;
   }
